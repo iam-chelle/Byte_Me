8f5e68e9422312f2f649728353c9cea7cca7a40a
rallagas <rallagas@bicol-u.edu.ph>
2021-04-01 00:28:25 +0800

    Update registration.php

--- registration.php
+++ registration.php
@@ -1,16 +1,6 @@
 				
 				<?php 
 				
-				/* POST VARIABLES: This part should be after the declaration of database connection*/
-				$fullname= $_POST['fname'];
-				$age= $_POST['age'];
-				$gender= $_POST['gender'];
-				$address=$_POST['add'];
-				$contact=$_POST['cnumber'];
-				$email=$_POST['email'];
-				$username=$_POST['uname'];
-				$password=$_POST['pass'];
-				/*END POST VARIABLES*/
 				
 				
 				/*DB CONNECTION: This part can now be called thru include "includes/db_conn.php"*/
@@ -25,6 +15,19 @@
 				die("Connection failed: " . mysqli_connect_error());
 				}
 				/*END DB CONNECTION*/
+				
+				
+				
+				/* POST VARIABLES: This part should be after the declaration of database connection*/
+				$fullname= $_POST['fname'];
+				$age= $_POST['age'];
+				$gender= $_POST['gender'];
+				$address=$_POST['add'];
+				$contact=$_POST['cnumber'];
+				$email=$_POST['email'];
+				$username=$_POST['uname'];
+				$password=$_POST['pass'];
+				/*END POST VARIABLES*/
 				
 				$sql = "INSERT INTO `cust_tbl` (`cust_name`, `cust_age`, `cust_gender`, `cust_add`, `cust_contact`, `cust_email`)
 				VALUES ( `${fullname}`, `${age}`, `${gender}`, `${address}`, `${contact}`, `${email}` );"; //<- use single quote on php variables inside an SQL. 
