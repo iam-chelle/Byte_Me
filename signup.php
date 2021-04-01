f414e7cb9e1b372c5bf53a8c3d129636bdf41e6f
Jeff Esguerra <81168894+JepKun@users.noreply.github.com>
2021-04-01 05:04:32 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -1,42 +1,46 @@
-			<?php
+<?php 
 
+	$fullname= $_POST['fname'];
+	$age= $_POST['age'];
+	$gender= $_POST['gender'];
+	$address=$_POST['add'];
+	$contact=$_POST['cnumber'];
+	$email=$_POST['email'];
+	$usname=$_POST['uname'];
+	$pasword=$_POST['pass'];
 
-			
-			$servername = "localhost";
-			$username = "";
-			$password = "";
-			$dbname = "practice";
+					
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "practice";
 
-			
-			// Create connection
-			$conn = new mysqli($servername, $username, $password, $dbname);
-			// Check connection
-			if ($conn->connect_error) {
-			  die("Connection failed: " . $conn->connect_error);
-			}
+					
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+				
+	if (!$conn) {
+	die("Connection failed: " . mysqli_connect_error());
+	}
 
-			$fullname= $_POST['fname'];
-			$age= $_POST['age'];
-			$gender= $_POST['gender'];
-			$address=$_POST['add'];
-			$contact=$_POST['cnumber'];
-			$email=$_POST['email'];
-			$username=$_POST['uname'];
-			$pasword=$_POST['pass'];
+				
+	$sql = "INSERT INTO `cust_tbl` (cust_name, cust_age, cust_gender, cust_add, cust_contact, cust_email)
+	VALUES ( '${fullname}', '${age}', '${gender}', '${address}', '${contact}', '${email}' );"; //<- use single quote on php variables inside an SQL. 
+	$sql2 = "INSERT INTO `user_tbl` (username, password)
+	VALUES ('${usname}', '${pasword}');"; 
 
+	if (mysqli_query($conn, $sql)) {
+	// if no error. Then new record created.
+	echo "New record created successfully";
+	} else {
+	// else then error will show up.
+	echo "Error: " . $sql . mysqli_error($conn);
+	}
 
-			
-
-			$sql = "INSERT INTO cust_tbl (cust_name, cust_age, cust_gender, cust_add, cust_contact, cust_email)
-			VALUES ( `${fullname}`, `${age}`, `${gender}`, `${address}`, `${contact}`, `${email}` );";
-			$sql .= "INSERT INTO user_tbl (username, password)
-			VALUES (`${username}`, `${pasword}`);";
-
-			if ($conn->multi_query($sql) === TRUE) {
-			  echo "New records created successfully";
-			} else {
-			  echo "Error: " . $sql . "<br>" . $conn->error;
-			}
-
-			$conn->close();
-			?>
+	if (mysqli_query($conn, $sql2)) {
+	// if no error. Then new record created.
+	echo "<br>New record created successfully";
+	} else {
+	// else then error will show up.
+	echo "Error: " . $sql2 . mysqli_error($conn);
+	}
+?>
