3a782e0f199ca546e8a5a609b909ad11def71dc3
Jeff Esguerra <81168894+JepKun@users.noreply.github.com>
2021-04-02 06:43:24 -0700

    Update signin.php

--- signin.php
+++ signin.php
@@ -11,7 +11,7 @@
 		<head>
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<link rel="stylesheet" type="text/css" href="css/account.css">
+			<link rel="stylesheet" type="text/css" href="css/style.css">
 			<title>Sue and Venir</title>
 			<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600&display=swap" rel="stylesheet">
 			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
