9d580925d47945f0e2d84a902be9bb6bdb465d4d
Jeff Esguerra <81168894+JepKun@users.noreply.github.com>
2021-04-27 20:36:03 -0700

    Update db_conn.php

--- Sue and Venir/includes/db_conn.php
+++ Sue and Venir/includes/db_conn.php
@@ -1,16 +1,16 @@
 				<?php
-				$servername = "localhost";
-				$dbusername = "root";
-				$dbpassword = "";
-				$dbname = "byteme";
-				
-				$conn = mysqli_connect($servername, $dbusername, $dbpassword, $dbname);
+								
 
-				if (!$conn) {
-				die("Connection failed: " . mysqli_connect_error());
-			}
-				else{
-					echo "";
+				$servername="localhost";
+				$dbusername="root";
+				$dbpassword="";
+				$dbname="byteme";
+
+				$conn = mysqli_connect($servername,$dbusername,$dbpassword,$dbname);
+
+				// Check connection
+				if (!$conn){
+				    die("Maintenance Mode.");
 				}
-				
+
 	
